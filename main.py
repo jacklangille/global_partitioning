dd2cf618cd33dc9fe4ce6723540a05826d50a128
Jack Langille <lwj1396@proton.me>
2025-02-12 18:17:52 -0400

    update ne asia

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ if __name__ == "__main__":
     with open("studies/hab_studies.json", "r") as f:
         studies = json.load(f)
 
-    for i in range(len(studies)):
-        name = studies[i]["name"]
-        cells = studies[i]["cells"]
+        name = studies[3]["name"]
+        cells = studies[3]["cells"]
         ndvi_dict = {cell_id: random.random() for cell_id in cells}
         new_study = HabStudy(cells, name)
         new_study.create_map()
