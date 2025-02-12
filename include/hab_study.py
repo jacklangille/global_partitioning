20b08fc77a13e7539e36daca4742b5cd92a5ea78
Jack Langille <lwj1396@proton.me>
2025-02-12 18:04:37 -0400

    update to allow entry of h3 cells

--- include/hab_study.py
+++ include/hab_study.py
@@ -2,81 +2,44 @@ import h3
 import folium
 import webbrowser
 import os
+from datetime import datetime
 
 
 class HabStudy:
     def __init__(
         self,
-        lat: float,
-        lng: float,
-        resolution: int = 10,
-        k: int = 45,
-        buffer_rad_m: int = 10e3,
+        cells: list,
         study_name: str = None,
     ):
-        self.lat: float = lat
-        self.lng: float = lng
-        self.res: int = resolution
-        self.hex_rad: int = k
-        self.buffer_rad: int = buffer_rad_m
+        self.cells: list = cells
         self.study_name: str = study_name
 
-    def _get_center_cell(self):
-        return h3.latlng_to_cell(self.lat, self.lng, self.res)
-
-    def _get_cluster_cells(self):
-        center = self._get_center_cell()
-        return h3.grid_disk(center, self.hex_rad)
-
     def _save_map(self, m):
-
-        if self.study_name:
-            map_file = f"output/maps/{self.study_name}.html"
-        else:
-            map_file = f"output/maps/{self.lat}_{self.lng}.html"
+        map_file = f"output/maps/{self.study_name}.html"
         m.save(map_file)
         full_path = os.path.abspath(map_file)
         url = f"file://{full_path}"
-
         webbrowser.open(url)
 
-    def create_map(self, zoom: int = 10):
-        m = folium.Map(location=[self.lat, self.lng], zoom_start=zoom)
-        cluster_cells = self._get_cluster_cells()
+    def create_map(self, zoom: int = 3):
+        lat, lng = h3.cell_to_latlng(self.cells[0])
 
-        for cell_id in cluster_cells:
+        m = folium.Map(location=[lat, lng], zoom_start=zoom)
+
+        for cell_id in self.cells:
             boundary = h3.cell_to_boundary(cell_id)
             folium.Polygon(
                 locations=boundary, color="red", fill=True, fill_opacity=0.2
             ).add_to(m)
 
-        folium.Circle(
-            location=[self.lat, self.lng],
-            radius=self.buffer_rad,
-            color="red",
-            fill=False,
-            weight=2,
-        ).add_to(m)
-
         self._save_map(m)
 
-    def save_cells(self):
-        if self.study_name:
-            cells_db = f"output/text/{self.study_name}.txt"
-        else:
-            cells_db = f"output/text/{self.lat}_{self.lng}.txt"
+    def save_cell_ndvi(self, ndvi_dict):
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        outfile = f"output/text/{self.study_name}_cells_{timestamp}.txt"
 
-        cluster_cells = self._get_cluster_cells()
-        with open(cells_db, "w") as f:
-            for cell_id in cluster_cells:
-                # Get the cell's own center lat/lng
-                cell_lat, cell_lng = h3.cell_to_latlng(cell_id)
-                f.write(f"{cell_id} {cell_lat} {cell_lng}\n")
-
-    def check_coord(self, lat_test: float, lng_test: float):
-        test_cell = h3.latlng_to_cell(lat_test, lng_test, self.res)
-
-        # Compare against our cluster set
-        if test_cell in self._get_cluster_cells():
-            return test_cell
-        return None
+        with open(outfile, "w") as f:
+            for cell_id in self.cells:
+                ndvi = ndvi_dict[cell_id]
+                lat, lng = h3.cell_to_latlng(cell_id)
+                f.write(f"{cell_id}, {lat}, {lng}, {ndvi:.4f}\n")
--- main.py
+++ main.py
@@ -1,33 +1,16 @@
 from include.hab_study import HabStudy
-import json
 import random
+import json
 
 if __name__ == "__main__":
 
     with open("studies/hab_studies.json", "r") as f:
         studies = json.load(f)
 
-    n_trials = 10
-
-    for study in studies:
-        new_study = HabStudy(
-            study["lat"],
-            study["lng"],
-            study["resolution"],
-            study["k"],
-            study["buffer_rad_m"],
-            study["name"],
-        )
-
-        print("-" * 40 + study["name"] + "-" * 40)
-        for i in range(n_trials):
-            test_lat = random.randrange(36, 40, 1)
-            test_lng = random.randrange(-120, -125, -1)
-            cell_found = new_study.check_coord(test_lat, test_lng)
-
-            print(
-                f"({test_lat}, {test_lng}) {'in ' + str(cell_found) if cell_found else 'NOT in the cluster'}"
-            )
-
-        # new_study.create_map()
-        # new_study.save_cells()
+    for i in range(len(studies)):
+        name = studies[i]["name"]
+        cells = studies[i]["cells"]
+        ndvi_dict = {cell_id: random.random() for cell_id in cells}
+        new_study = HabStudy(cells, name)
+        new_study.create_map()
+        new_study.save_cell_ndvi(ndvi_dict)
